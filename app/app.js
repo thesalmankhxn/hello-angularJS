b0ef2b5dae7f790996dfab28acf2113c060c5ea8
iamSK77 <sk77.mobilefix@gmail.com>
2020-10-08 15:11:06 +0530

    initial commit

--- app/app.js
+++ app/app.js
@@ -3,3 +3,19 @@ var myBBApp = angular.module('myBBApp', []);
 myBBApp.config(function(){
 
 });
+
+var MainController = function($scope, $http){
+
+    var onUserComplete = function(response) {
+        $scope.user = response.data;
+    };
+
+    var onError = function(err) {
+        $scope.error = "Could not retrieve data from the api"
+    };
+
+    $http.get("https://api.github.com/users/iamSK77")
+        .then(onUserComplete, onError);
+
+    $scope.message = "Hello, AngularJS!";
+};
