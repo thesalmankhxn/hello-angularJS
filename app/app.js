37c0bd36165cd2bdc1d8ae5225c5dba4b12a377d
iamSK77 <sk77.mobilefix@gmail.com>
2020-10-09 12:27:10 +0530

    filter

--- app/app.js
+++ app/app.js
@@ -10,6 +10,20 @@ myBBApp.config(function () {
 
     var MainController = function ($scope, $http) {
 
+        $scope.ninjas = [
+            {
+                name: 'Yoshi'
+        },
+        {
+            name: 'Crystal'
+        },
+        {
+            name: 'Ryu'
+        },
+        {
+            name: 'Shawn'
+    }];
+
         var onUserComplete = function (response) {
             $scope.user = response.data;
         };
@@ -18,20 +32,22 @@ myBBApp.config(function () {
             $scope.error = "Could not retrieve data from the api"
         };
 
-        $http.get("https://api.github.com/users/iamSK77")
+        $http.get(`https://api.github.com/users/iamSK77`)
             .then(onUserComplete, onError);
 
         $scope.message = "Hello, AngularJS!";
     };
 
+
+
     app.controller("MainController", [ '$scope', '$http', MainController]);
 
 }());
 
-var myNinjaApp = angular.module('myNinjaApp', []);
+// var myNinjaApp = angular.module('myNinjaApp', []);
 
-myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+// myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 
-    $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shawn'];
-    
-}]);
+//     $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shawn'];
+
+// }]);
